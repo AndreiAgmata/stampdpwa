91e25465b55e278b53941f5bf57f0e80adc40dbe
AndreiAgmata <agmataa1124@gmail.com>
2024-02-02 02:52:34 -0500

    Added temporary login  button for mobile

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -9,6 +9,13 @@ function NavBar() {
     <section className="navbar-section">
       <div className="container-md h-100 d-flex align-items-center justify-content-between">
         <h1 className="title">stampd</h1>
+        <button
+          type="button"
+          className="btn btn-light"
+          onClick={() => router.push("/account/login")}
+        >
+          Login
+        </button>
         <div className="links d-lg-flex d-md-flex gap-3 align-items-center d-sm-none d-none ">
           <Link href={"https://stampd.ca"}>About</Link>
           <Link href={"./account/login"}>Client Login</Link>
